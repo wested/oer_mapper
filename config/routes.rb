dd81a4afba8c6b6ba304f23143b9495ef48d1c26
May Miller-Ricci <maymillerricci@gmail.com>
2014-02-25 10:23:38 -0800

    rails_apps_composer: models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 OerMapper::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
