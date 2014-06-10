924b741d3908dae152f7dd3779b99756b27224f5
May Miller-Ricci <maymillerricci@gmail.com>
2014-06-10 10:58:05 -0700

    change org and user models for has many through

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,12 @@
 class Organization < ActiveRecord::Base
   has_one :address
-  has_and_belongs_to_many :users
+
+  has_many :organizations_users
+  has_many :users, through: :organizations_users
+
+  # TODO: possibly change these to has many through
   has_and_belongs_to_many :languages
   has_and_belongs_to_many :subjects
-  #TODO: validation that only 'Serendipity user' can modify these fields
+  # TODO: validation that only 'Serendipity user' can modify these fields
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_and_belongs_to_many :organizations
+  has_many :organizations_users
+  has_many :organizations, through: :organizations_users
 end
